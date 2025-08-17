c912a3ba3f0f5ae05b8a34c8555557a529896402
hunghg255 <giahunghust@gmail.com>
2025-08-17 10:57:01 +0700

    chore: update

--- src/enums/file.enum.ts
+++ src/enums/file.enum.ts
@@ -1,16 +1,16 @@
 export enum FileFormat {
   "JSON" = "json",
-  "YAML" = "yaml",
-  "XML" = "xml",
-  "TOML" = "toml",
-  "CSV" = "csv",
+  // "YAML" = "yaml",
+  // "XML" = "xml",
+  // "TOML" = "toml",
+  // "CSV" = "csv",
 }
 
 export const formats = [
   { value: FileFormat.JSON, label: "JSON" },
-  { value: FileFormat.YAML, label: "YAML" },
-  { value: FileFormat.XML, label: "XML" },
-  { value: FileFormat.CSV, label: "CSV" },
+  // { value: FileFormat.YAML, label: "YAML" },
+  // { value: FileFormat.XML, label: "XML" },
+  // { value: FileFormat.CSV, label: "CSV" },
 ];
 
 export enum TypeLanguage {
